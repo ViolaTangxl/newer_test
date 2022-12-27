1dca4a056efb0dc58dd4406a585ebdd3179a53db
violaTangxl <tangxuelei@qimao.com>
2022-12-27 15:37:00 +0800

    feat: 定义articleHandler 和接口

---
+++ app/controller/article.go
@@ -0,0 +1,35 @@
+package controller
+
+import (
+	"context"
+	"github.com/ViolaTangxl/newer_test/models"
+	"github.com/gin-gonic/gin"
+)
+
+type ArticleHandler struct {
+	ctx context.Context
+	mgr *models.ArticleMgr
+}
+
+// NewArticleHandler 新建文章handler
+func NewArticleHandler(ctx context.Context, mgr *models.ArticleMgr) *ArticleHandler {
+	return &ArticleHandler{
+		ctx: ctx,
+		mgr: mgr,
+	}
+}
+
+// GetArticlesList 获取文章列表
+func (h *ArticleHandler) GetArticlesList(ctx *gin.Context) {
+	// TODO
+}
+
+// AddArticles 新增文章
+func (h *ArticleHandler) AddArticles(ctx *gin.Context) {
+	// TODO
+}
+
+// UpdateArticles 文章编辑
+func (h *ArticleHandler) UpdateArticles(ctx *gin.Context) {
+	// TODO
+}
