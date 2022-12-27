9fdb9bb7eb0e5586b73c1e51f99205a6a5afb5bb
violaTangxl <tangxuelei@qimao.com>
2022-12-27 16:13:33 +0800

    feat: 程序入口增加相关逻辑调用

--- app/main.go
+++ app/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"github.com/ViolaTangxl/newer_test/config"
+	"github.com/ViolaTangxl/newer_test/env"
 	"github.com/sirupsen/logrus"
 )
 
@@ -13,9 +15,26 @@ func main() {
 	flag.StringVar(&configPath, "conf", "config.yml", "config file path")
 	flag.Parse()
 
-	_, err := config.LoadConfig(configPath)
+	conf, err := config.LoadConfig(configPath)
 	if err != nil {
 		logrus.WithError(err).Fatal("failed to load config")
 	}
 
+	ctx := context.Background()
+	logger := logrus.StandardLogger()
+	env.Global.Cfg = conf
+	env.Global.Logger = logger
+	env.Global.ArticleMgr = env.InitMongo(ctx, logger, conf)
+
+	// 初始化项目配置
+	app := env.InitApp(logger, conf)
+
+	// 绑定路由
+	env.InitRouters(app)
+
+	// TODO 抽取配置文件
+	err = app.Run("11009")
+	if err != nil {
+		logger.WithError(err).Fatal("service run failed")
+	}
 }
